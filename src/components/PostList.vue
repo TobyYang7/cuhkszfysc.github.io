342ae412aea21dec5ce2b247a9dc6e06124609b8
Diantong Li <cuhkszfysc@outlook.com>
2026-04-10 19:41:08 +0800

    Update PostList.vue

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -3,15 +3,31 @@ import METADATA from "../metadata";
 import type { Page } from "../pages.data";
 
 defineProps<{ posts: Page[] }>();
+
+function formatPostMeta(post: Page) {
+  const metadata = post.metadata as {
+    type: keyof typeof METADATA.type;
+    school?: keyof typeof METADATA.school;
+    year?: string | number;
+  };
+
+  const parts = [
+    METADATA.type[metadata.type],
+    metadata.school ? METADATA.school[metadata.school] : undefined,
+    metadata.year
+      ? METADATA.year[String(metadata.year).padStart(2, "0")]
+      : undefined,
+  ].filter((part): part is string => Boolean(part));
+
+  return `[${parts.join("，")}]`;
+}
 </script>
 
 <template>
   <ul>
     <li v-for="post in posts" :key="post.url">
       {{ post.metadata.date }} -
-      <!-- FIXME: need to validate metadata type -->
-      <!-- @vue-expect-error need to type this -->
-      {{ `[${METADATA.type[post.metadata.type]}]${post.metadata.school ? `[${METADATA.school[post.metadata.school]}]` : ""}` }}
+      {{ formatPostMeta(post) }}
       -
       <a :href="post.url">{{ post.title ?? "无标题" }}</a>
     </li>
